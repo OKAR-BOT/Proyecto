d6c11d8d650bbdfd12f4a0fe9a911dc5bc0228c0
LAB-USR-LCENTRO <LAB-USR-LCENTRO@grupoutp.net>
2025-08-22 19:48:57 -0500

    Se creo el proyecto LandPage en Springboot

---
+++ LandPage/src/main/java/com/utp/LandPage/LandPageApplication.java
@@ -0,0 +1,13 @@
+package com.utp.LandPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandPageApplication.class, args);
+	}
+
+}
